64d41c0b4d923ce91c3f2c19863f2ea31d8ca1a2
mgramin <mgramin@gmail.com>
2017-04-19 16:43:20 +0300

    init

---
+++ src/test/java/PostgresCaseIT.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import java.sql.*;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by maksim on 19.04.17.
+ */
+public class PostgresCaseIT {
+
+    @Test
+    public void test() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres", "postgres")) {
+            Statement statement = connection.createStatement();
+            statement.execute("create table test_table (id integer)");
+            statement.execute("insert into test_table (id) values (1)");
+            statement.execute("insert into test_table (id) values (2)");
+            ResultSet resultSet = statement.executeQuery("select sum(id) as summ from test_table");
+            resultSet.next();
+            int summ = resultSet.getInt("summ");
+            assertEquals(3, summ);
+        }
+    }
+
+}
